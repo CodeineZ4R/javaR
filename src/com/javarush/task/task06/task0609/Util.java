f2a343d4798d89b76744162fa53bbd715bdfba5f
CodeineZ4R <codeine.z4r@gmail.com>
2017-08-16 19:39:52 +0300

    task 0609 done

--- src/com/javarush/task/task06/task0609/Util.java
+++ src/com/javarush/task/task06/task0609/Util.java
@@ -1,17 +1,14 @@
 package com.javarush.task.task06.task0609;
-import java.math.*;
 
 /* 
 Расстояние между двумя точками
 */
 
 public class Util {
-
     public static double getDistance(int x1, int y1, int x2, int y2) {
-        double rez = ((x2 - x1) ^ 2) + ((y2 - y1) ^ 2);
-        return Math.sqrt( rez);
+        double d = (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return Math.sqrt(d);
     }
-
 
 
     public static void main(String[] args) {
